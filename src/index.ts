417318be945cbe5c8625ed2970845b366e3f827b
AbooSalh <a.m0507979@gmail.com>
2025-03-04 20:36:32 +0200

    adding morgan middleware for logging API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
+// Importing necessary modules
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-// .env configuration
+import morgan from "morgan";
+
+// Configure environment variables
 dotenv.config({ path: ".env" });
+
+// Create an Express application
 const app = express();
+
+// Retrieve port from environment variables
 const port = process.env.PORT;
+
+// Use morgan middleware for logging in development environment
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Define a route handler for the root path
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
 });
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
