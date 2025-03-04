447e1374ab4d0880ec7b00800b804cc22774ab72
AbooSalh <a.m0507979@gmail.com>
2025-03-04 20:27:59 +0200

    nodemon setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
-
+import dotenv from "dotenv";
+// .env configuration
+dotenv.config({ path: ".env" });
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
 });
